306b4d1e9c20122875c0ee314084b358ade0461f
ALEXIS <anavarro776@gmail.com>
2025-05-12 11:34:25 -0400

    Test

---
+++ Variables.js
@@ -0,0 +1,63 @@
+console.log("Hello World Ch 58");
+
+// This a comment JS//
+
+console.log("Hello my name is Alex and I Am 26 years old");
+
+//STRING VARIABLES//
+
+let name = "Alex";
+
+//Keyword variablename =value;
+
+let lastName  ="Navarro";
+
+console.log(name);
+console.log(lastName);
+
+console.log("Hello my name is "+ name + " " + lastName);
+
+//Creating Numerical Variables
+
+let age = 40;
+let grade = 9.0;
+
+console.log(age);
+console.log("I am " + age + " years old");
+
+
+//boolean variables (true or false)
+let isAProfessor = true;
+let isAStudent = false;
+let isAdmin = true;
+console.log(isAProfessor);
+
+/* Many Lines Of Comment  */
+
+let number1= 8.3;
+let number2 = 4;
+let additon=number1 + number2;
+
+
+console.log(additon)
+console.log(number1 + " + " + number2+ " = " + (number1 + number2));
+
+//Subtraction
+let subtraction= number1-number2;
+
+console.log(subtraction);
+
+
+//  Multipecation
+
+let multipecation= number1*number2;
+
+console.log(number1+ " x " + number2 + " = " + multipecation);
+
+//Constant Variables
+
+const PI = 3.1416;
+const MYNAME = "Alex";
+
+console.log(PI);
+console.log(MYNAME);
---
+++ funny-story.js
@@ -0,0 +1,34 @@
+// Version One Of Story//
+
+const dog="Max";
+
+
+let owner="Tim";
+let object="shoe";
+let action=" laughed";
+let place ="park";
+let tempature = "sunny day";
+
+
+
+
+console.log("--Funny Story--");
+console.log("One "+ tempature + ", "+ owner + " took his dog, " + dog + " to the "+ place + " . As " + owner + " threw a stick " + dog +" came back with a random "  + object+".");
+console.log( owner + action + " as "+ dog + " proudly presented his find. Everyone in the park chuckled at the confused "+ dog + "."  );
+console.log( " From that day on, " + dog + " was known as the "+ object+ " thief of the "+ place + "!" );
+
+
+
+// Version Two Of Story//
+
+owner="Mary";
+object="piece of clothing";
+action=" was emberrased"; 
+place = " school";
+tempature="rainy day ";
+typeOfClothing=" shorts"
+
+console.log("--Funny Story V2--");
+console.log("One "+ tempature + ", "+ owner + " took her dog, " + dog + " to " + place + " . As " + owner + " threw a stick " + dog +" came back with a random "  + object+".");
+console.log( owner + action + " as "+ dog + " proudly presented his find. Everyone in the "+ place+" chuckled at the confused "+ dog + "."  );
+console.log( " From that day on, " + dog + " was known as the"+ typeOfClothing + " thief of the "+ place + "!" );
